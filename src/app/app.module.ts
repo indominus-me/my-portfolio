34dbe78dce9c5d2272c1d60e33e3f9aad12ddb9e
indominus_me <saqlainsha@gmail.com>
2019-02-24 15:36:04 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule,
+         MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentComponent } from './content/content.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
